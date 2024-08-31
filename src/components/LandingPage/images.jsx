91b3ede16afa3327b294593ec620268480140518
Younes-khiat <Khiatyounes27@gmail.com>
2024-08-31 04:14:46 +0100

    styling landing page

--- src/components/LandingPage/images.jsx
+++ src/components/LandingPage/images.jsx
@@ -4,7 +4,6 @@ import image3 from "./Pictures/photo_2024-07-20_18-03-13.jpg";
 import image4 from "./Pictures/photo_2024-07-20_18-08-09.jpg";
 import image5 from "./Pictures/photo_2024-07-20_18-08-15.jpg";
 import image6 from "./Pictures/photo_2024-07-20_18-08-39.jpg";
-
 const images = [image1, image2, image3, image4, image5, image6];
 
 export default images;
--- src/components/LandingPage/landingOage.jsx
+++ src/components/LandingPage/landingOage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import images from './images';
+import logo from './Pictures/logo2.png';
+
 const LandingPage = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -13,20 +15,21 @@ const LandingPage = () => {
   }, []);
 
   const navigate = useNavigate();
+
   const handleJoinUsClick = () => {
     navigate('/section1');
   };
 
   return (
-    <div className='d-flex justify-content-center w-100'>
-      <div className='w75'>
-          <img src={images[currentSlide]} alt="Slide" />
+    <div className=' w-100 h-100 d-flex'>
+      <div style={{width:'85%'}} className='h-100 position-absolute'>
+          <img className='h-100 img-fluid w-100' src={images[currentSlide]} alt="Slide" />
       </div>
-      <div className='w-25 d-flex justify-content-center'>
-        <img src="https://drive.google.com/file/d/1B24TIhySAN7lLfZO21x2_5PInkakjiwo/view?usp=sharing" alt="cscc logo" />
-          <h1 className='text-primary'>Hello there!</h1>
-          <h5>We are happy to see you here</h5>
-          <button onClick={handleJoinUsClick}>Join Us</button>
+      <div className='position-absolute end-0 h-100 d-flex justify-content-center align-items-center flex-column  my-auto  mx-auto px-5' style={{background: 'linear-gradient(to right, transparent 0%, #202068 35%, #202068 100%)', width: '40%'}} >
+          <img className='img-fluid w-50 p-3' src={logo} alt="cscc logo" />
+          <h2 className='text-primary text-center pt-4 pb-1'>Hello there!</h2>
+          <h5 style={{color: '#F6F5FC'}} className='text-center pt-1 pb-4 '>We are happy to see you here</h5>
+          <button className='btn btn-outline-light fw-bold' onClick={handleJoinUsClick}>Join Us</button>
       </div>
     </div>
   );
