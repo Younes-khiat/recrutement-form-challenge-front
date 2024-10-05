e94426c6ea67bc104628b6f244b41e4c584c2bd0
Younes Khiat <khiatyounes27@gmail.com>
2024-10-05 15:37:57 +0100

    responsive section3

--- src/components/section 3/section3.jsx
+++ src/components/section 3/section3.jsx
@@ -16,9 +16,9 @@ function SectionThree() {
 
   const [isReady, setIsReady] = useState(false);
   // State to store values for each range input
-  const [rangeValue1, setRangeValue1] = useState(1);
-  const [rangeValue2, setRangeValue2] = useState(1);
-  const [rangeValue3, setRangeValue3] = useState(1);
+  const [rangeValue1, setRangeValue1] = useState(3);
+  const [rangeValue2, setRangeValue2] = useState(3);
+  const [rangeValue3, setRangeValue3] = useState(3);
   const getRangeLabel = (value) => {
     if (value == 0) return "I don't want to";
     if (value == 1) return "not really";
@@ -53,7 +53,7 @@ useEffect(() => {
 }, [isReady]);
 
   return (
-  <div className=' position-absolute custom-height w-100 d-flex'>
+  <div className='position-absolute custom-height w-100 d-flex'>
     <div className='widthColor1 d-none d-md-flex px-5 h-100 flex-column justify-content-center'>
         <img className='img-fluid w-50 mb-5 mx-auto ' src={logo} alt="cscc logo" />
         <h4 className='text-light text-center mt-5 mb-5'>Registration Form for joining cscc club 2024 / 2025</h4>
@@ -61,34 +61,35 @@ useEffect(() => {
         <button onClick={goBack} className='btn btn-outline-light fw-bold mx-auto my-2'>go back</button>
     </div>
     <div className="custom-width widthColor2  d-flex flex-column justify-content-center">
-      <h2 className='mx-5 px-4 py-4 fw-bold text-dark'>Preferences</h2>
-       <form onSubmit={handleSubmit} className='row g-3 needs-validation '>
-        <div className=" col-md-12 row">
-          <h6 className='col-4'>Development Dep :
+      <h2 className='mx-5 my-4 my-sm-3 px-4 py-4 fw-bold text-dark'>Preferences:</h2>
+      <label htmlFor='choosingDepartement' className='form-label fw-semibold fs-5 mb-4 d-none d-sm-block'>Choose Your Departement:</label>
+       <form onSubmit={handleSubmit} className='row g-3 bg-white needs-validation mobileBorder rounded-4 pt-3'>
+        <div className=" col-md-12 row py-2">
+          <h6 className='col-md-4'>Development Dep:
           </h6>
-          <div className="col-5">
+          <div className="col-md-5">
             <input className='form-range' type="range" onChange={(event)=>{handleChange(event); setRangeValue1(event.target.value);}} min="0" max="3"></input>
           </div>
-          <h6 className='col-3'>{getRangeLabel(rangeValue1)}</h6>
+          <h6 className='col-md-3 text-center py-2 py-sm-0'>{getRangeLabel(rangeValue1)}</h6>
         </div>
-        <div className=" col-md-12 row">
-          <h6 className='col-4'>Communication Dep :
-
-          </h6>
-          <div className="col-5">
+        <div className="d-block d-sm-none my-3 w-100 mx-auto" style={{height: '2px',backgroundColor:'#202068'}}></div>
+        <div className=" col-md-12 row py-2">
+          <h6 className='col-md-4'>Communication Dep:</h6>
+          <div className="col-md-5">
             <input className='form-range' type="range" onChange={(event)=>{handleChange(event); setRangeValue2(event.target.value);}} min="0" max="3"></input>
           </div>
-          <h6 className='col-3'>{getRangeLabel(rangeValue2)}</h6>
+          <h6 className='col-md-3 text-center py-2 py-sm-0'>{getRangeLabel(rangeValue2)}</h6>
         </div>
-        <div className=" col-md-12 row">
-          <h6 className='col-4'>Robotics Dep :
-          </h6>
-          <div className="col-5">
+        <div className="d-block d-sm-none my-3 w-100 mx-auto" style={{height: '2px',backgroundColor:'#202068'}}></div>
+        <div className=" col-md-12 row py-2">
+          <h6 className='col-md-4'>Robotics Dep:</h6>
+          <div className="col-md-5">
             <input className='form-range' type="range" onChange={(event)=>{handleChange(event); setRangeValue3(event.target.value);}} min="0" max="3"></input>
           </div>
-          <h6 className='col-3'>{getRangeLabel(rangeValue3)}</h6>
+          <h6 className='col-md-3 text-center py-2 py-sm-0'>{getRangeLabel(rangeValue3)}</h6>
         </div>
-         <button type="submit" className='form-control my-4 py-2 btn btn-custom fw-bold fs-5'>Next</button>
+        <div className="d-block d-sm-none"></div>
+         <button type="submit" className='form-control mt-0 mt-sm-3 pt-2 btn btn-custom fw-bold fs-5'>Next</button>
          </form>
     </div>
     
